6ead1073d40f202ea2f184c14bc7ce7cfb04589f
Sumeet Tiwari <tiwari.sumeet@gmail.com>
2018-03-17 03:43:33 +0530

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,27 @@ myImage.onclick = function() {
       myImage.setAttribute ('src','images/firefox-icon.png');
     }
 }
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+function setUserName() {
+    var myName = prompt('Please enter Name');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+if(!localStorage.getItem('name'))
+{
+    setUserName();
+}
+else 
+{
+    var storedName = localStorage.getItem('name');
+   myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+myButton.onclick =function()
+{
+    setUserName();
+}
+
+                                          
+                                          
+    
